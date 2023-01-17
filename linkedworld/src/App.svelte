4984dbc592aa4f91080727504916b9052cd6208f
lonycell <lonycell@gmail.com>
2023-01-17 12:15:49 +0900

    v0.1

--- linkedworld/src/App.svelte
+++ linkedworld/src/App.svelte
@@ -17,14 +17,6 @@
   <div class="card">
     <Counter />
   </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
@@ -38,8 +30,5 @@
   }
   .logo.svelte:hover {
     filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
   }
 </style>
